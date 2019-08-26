ab44f565f0763fba2a8f4adf0f3951441955936e
Phoebe Law <phoebebasilio@gmail.com>
2019-08-25 20:21:12 -0700

    added api.js. have most of index.html. minor js changes in progress.

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+/* Notes: 
+
+- you need a {} set after a fat arrow
+- pull out of a legend within same index, not a datastore. necessary for accessibility.
+
+*/
+
+'use strict';
+
+const store = (function () {
+
+
+})();
+// if var from another link/sheet, use the following and replace cuid w/ the other var
+// global strftime, cuid
---
+++ scripts/store.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const store = (function () {
+  const createAccordianHtml = function() {
+    <button class="accordion">Section 1</button>
+    <div class="panel">
+      <p>Lorem ipsum...</p>
+    </div>
+    
+  };
+
+  return {
+    createAccordianHtml
+  }
+})();
